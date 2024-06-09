aaaa59a6bceda3c873ed4ca890270987d59fdd3f
breadlol64 <101391903+breadlol64@users.noreply.github.com>
2024-06-09 12:12:10 +0300

    Add files via upload

--- bober/index.js
+++ bober/index.js
@@ -2,3 +2,10 @@ let tg = window.Telegram.WebApp
 
 let name_el = document.getElementById("name")
 name_el.innerHTML = tg.initDataUnsafe.user.first_name
+tg.expand()
+
+function addScore() {
+    let score_el = document.getElementById("score")
+    score_el.innerHTML = Number(score_el.innerText) + 1
+    
+}
