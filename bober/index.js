9fb5ac10cf4a190111ef8380b4c2336f0c2756fb
breadlol64 <101391903+breadlol64@users.noreply.github.com>
2024-07-27 11:44:39 +0300

    Add files via upload

--- bober/index.js
+++ bober/index.js
@@ -1,11 +1,25 @@
-let tg = window.Telegram.WebApp
-
-let name_el = document.getElementById("name")
-name_el.innerHTML = tg.initDataUnsafe.user.first_name
-tg.expand()
+function getCookie(name) {
+    let nameEQ = name + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
 
 function addScore() {
     let score_el = document.getElementById("score")
     score_el.innerHTML = Number(score_el.innerText) + 1
-    
+    userid = getCookie("userid")
+    fetch(`http://127.0.0.1:6464/setcoins?userid=${userid}&coins=${score_el.value}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json(); // parses JSON response into native JavaScript objects
+        })
+        .then(data => console.log(data))
+        .catch(error => console.error('There has been a problem with your fetch operation:', error));
 }
