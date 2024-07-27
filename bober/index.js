018f52ae163563e63ed3930edbc9bae178a00c84
breadlol64 <101391903+breadlol64@users.noreply.github.com>
2024-07-27 12:33:00 +0300

    Add files via upload

--- bober/index.js
+++ bober/index.js
@@ -1,3 +1,21 @@
+let score_el = document.getElementById("score")
+const userid = getCookie("userid")
+const username = getCookie("username")
+const password = getCookie("password")
+
+fetch(`http://127.0.0.1:6464/user/${username}?password=${password}`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return response.json(); // parses JSON response into native JavaScript objects
+        })
+        .then(data => {
+          console.log(data)
+          score_el.innerText = data["coins"]
+        })
+        .catch(error => console.error('There has been a problem with your fetch operation:', error));
+
 function getCookie(name) {
     let nameEQ = name + "=";
     let ca = document.cookie.split(';');
@@ -10,9 +28,7 @@ function getCookie(name) {
 }
 
 function addScore() {
-    let score_el = document.getElementById("score")
     score_el.innerHTML = Number(score_el.innerText) + 1
-    userid = getCookie("userid")
     fetch(`http://127.0.0.1:6464/setcoins?userid=${userid}&coins=${score_el.innerText}`)
         .then(response => {
           if (!response.ok) {
